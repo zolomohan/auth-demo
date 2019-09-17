d019fe6a3c02708a3eddc9a5c854e9b0ee36935c
unknown <mohanzolomon@gmail.com>
2019-09-17 03:05:02 -0700

    Upload Files

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express'),
+			mongoose = require('mongoose'),
+			passport = require('passport'),
+			localStrategy = require('passport-local'),
+			bodyParser = require('body-parser'),
+			User = require('./models/user');
+
+const server = express();
+
+mongoose.connect('mongodb://localhost/auth_demo', {
+	useNewUrlParser: true
+});
+
+server.set('view engine', 'ejs');
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(
+	require('express-session')({
+		secret: 'Rusty is the best and cutest dog in the world',
+		resave: false,
+		saveUninitialized: false
+	})
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+server.get('/', (req, res) => res.render('home'));
+server.get('/login', (req, res) => res.render('login'));
+server.get('/register', (req, res) => res.render('register'));
+server.get('/secret', isLoggedIn, (req, res) => res.render('secret'));
+
+server.post('/register', (req, res) => {
+	User.register(
+		new User({ username: req.body.username }),
+		req.body.password,
+		(error) => {
+			if (error) return res.render('register');
+			passport.authenticate('local')(req, res, () => res.redirect('/secret'));
+		}
+	);
+});
+
+server.post(
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/secret',
+		failureRedirect: '/login'
+	})
+);
+
+server.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	return res.redirect('/login');
+}
+
+server.listen(3000, () => console.log('Auth Server has started'));
